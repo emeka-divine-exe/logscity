13ee52b29c33c8d1b50a1b73c18f3514ebdfeb88
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-01 23:04:17 +0100

    Create Index.ts to export shared components

---
+++ src/components/shared/Index.ts
@@ -0,0 +1,7 @@
+export { Logo } from './Logo';
+export { PageHeader } from './PageHeader';
+export { PlatformIcon } from './PlatformIcon';
+export { SearchBar } from './SearchBar';
+export { FilterBar } from './FilterBar';
+export { StatisticsCard } from './StatisticsCard';
+export { StatisticsGrid } from './StatisticsGrid';
