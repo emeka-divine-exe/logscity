dea87d59b5fb29a6dcf83cfdbd0c78feee8e1249
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-03 17:31:22 +0100

    Export additional components from landing index

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,1 +1,6 @@
+export { Navbar } from './Navbar';
+export { Hero } from './Hero';
 export { FeaturedListings } from './FeaturedListings';
+export { Testimonials } from './Testimonials';
+export { Faq } from './Faq';
+export { Footer } from './Footer';
