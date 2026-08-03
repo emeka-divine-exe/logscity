cae30a4fe575511f7f787760a16a6515e091fc64
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-03 17:56:33 +0100

    Create HomePage component with landing sections

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,12 @@
+import { Hero, FeaturedListings, Testimonials, Faq } from '@/components/landing';
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedListings />
+      <Testimonials />
+      <Faq />
+    </>
+  );
+}
