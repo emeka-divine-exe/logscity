afc25eb2e75c1d6a5572066efd5eb716c15eb438
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-03 22:38:24 +0100

    Rename Index.ts to index.ts

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,7 +1,8 @@
 export { Logo } from './Logo';
-export { PageHeader } from './PageHeader';
+/*export { PageHeader } from './PageHeader';
 export { PlatformIcon } from './PlatformIcon';
 export { SearchBar } from './SearchBar';
 export { FilterBar } from './FilterBar';
 export { StatisticsCard } from './StatisticsCard';
 export { StatisticsGrid } from './StatisticsGrid';
+*/
