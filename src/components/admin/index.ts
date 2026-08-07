624344b5f69bfa127cb6a1e411456d08aa55539c
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-07 22:28:34 +0100

    Add new exports for admin components

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -1,2 +1,5 @@
-export { CategoriesList } from './CategoriesList';
 export { CategoryFormModal } from './CategoryFormModal';
+export { AccountsManageModal } from './AccountsManageModal';
+export { AddAccountModal } from './AddAccountModal';
+export { AvailableAccountsClient } from './AvailableAccountsClient';
+export { AdminDashboardClient } from './AdminDashboardClient';
