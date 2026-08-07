79079d2ebd685543eeab4c65087f220f6bc14c09
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-07 11:30:59 +0100

    Export AdminShell from layout index

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { Sidebar } from './Sidebar';
 export { AuthenticatedShell } from './AuthenticatedShell';
 export { AdminSidebar } from './AdminSidebar';
+export { AdminShell } from './AdminShell';
