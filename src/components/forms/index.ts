e8326d10c64c84b25bbf3e77af4a016ef9eedb66
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-07 10:37:14 +0100

    Export ChangePasswordForm and SettingsForm components

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,2 +1,4 @@
 export { RegisterForm } from './RegisterForm';
 export { LoginForm } from './LoginForm';
+export { ChangePasswordForm } from './ChangePasswordForm';
+export { SettingsForm } from './SettingsForm';
