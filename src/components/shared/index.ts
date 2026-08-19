d8063979e8b5e0aec967966b38485c32456f3448
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-20 00:14:40 +0100

    Refactor index.ts to clean up exports
    Removed commented-out exports and added FloatingWhatsApp export.

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,9 +1,4 @@
 export { Logo } from './Logo';
 export { BackButton } from './BackButton';
-/*export { PageHeader } from './PageHeader';
-export { PlatformIcon } from './PlatformIcon';
-*/ export { SearchBar } from './SearchBar';/*
-export { FilterBar } from './FilterBar';
-export { StatisticsCard } from './StatisticsCard';
-export { StatisticsGrid } from './StatisticsGrid';
-*/
+export { SearchBar } from './SearchBar';
+export { FloatingWhatsApp } from './FloatingWhatsApp';
