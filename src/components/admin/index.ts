5825ce7573f67d9afd594f9219eed29e3f1f1714
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-30 14:26:11 +0100

    Export WarningsSection from admin index

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -4,3 +4,4 @@ export { AddAccountModal } from './AddAccountModal';
 export { AvailableAccountsClient } from './AdminAccountClient';
 export { AdminDashboardClient } from './AdminDashboardClient';
 export { PendingOrdersSection } from './PendingOrdersSection';
+export { WarningsSection } from './WarningsSection';
